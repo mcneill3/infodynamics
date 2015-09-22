a844726ec2325ac5ba1dcd4b0a53105e46871d6e
Colin McNeill <colinpmcneill@gmail.com>
2015-09-22 15:54:09 -0400

    removed turbo, fixed delete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-= require jquery
-= require jquery_ujs
+//= require jquery
+//= require jquery_ujs
 //= require twitter/bootstrap
-= require twitter/bootstrap
-//= require turbolinks
 //= require_tree .
