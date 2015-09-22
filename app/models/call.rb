9d04b681a499f863ca06d7d55f0831b09fe5b643
Colin McNeill <colinpmcneill@gmail.com>
2015-09-22 16:54:08 -0400

    link for customer in calls and emails

--- app/models/call.rb
+++ app/models/call.rb
@@ -1,3 +1,5 @@
 class Call < ActiveRecord::Base
   belongs_to :customer
+  validates_presence_of :customer
+
 end
--- app/models/email.rb
+++ app/models/email.rb
@@ -1,3 +1,4 @@
 class Email < ActiveRecord::Base
   belongs_to :customer
+  validates_presence_of :customer
 end
